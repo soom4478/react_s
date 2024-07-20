f911708cd1f8eaca8465108b144bf2b0baf25f7d
soomin <s2305@e-mirim.hs.kr>
2024-07-20 19:47:04 +0900

    6-2 useEffect_2

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
   useEffect(() => {
-    console.log("ni fun on");
+    console.log("i run only once.");
   }, []);
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
+    console.log("i run when 'keyword' changes");
   }, [keyword]);
-
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when keyword & counter changes");
+  }, [keyword, counter]);
   return (
     <div>
       <input
